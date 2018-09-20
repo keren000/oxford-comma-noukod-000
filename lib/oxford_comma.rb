1c0d93e3e586a7b33b99cb9d52b33997ca12a194
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-20 13:17:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,10 @@
 def oxford_comma(array)
   if array.length < 2
-    array.join 
+    array.join
   elsif array.length == 2
-    array.insert(1, "and").join(" ") 
-  else 
+    array.insert(1, "and").join(" ")
+  else
     str = ", and " + array.pop
-    str = array.join(", ") << str 
-  end 
-end 
+    str = array.join(", ") << str
+  end
+end
